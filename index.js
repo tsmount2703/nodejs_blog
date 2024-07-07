90121fc6e3c790d0895c9251076ee86cead51c1a
Trung Son <trungsonvonguyen@gmail.com>
2024-07-07 16:44:12 +0700

    Remove src/public/css folder

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 5000;
-
-app.get("/trang-chu", (req, res) => res.send("Hello World!!I am Trung Son"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
